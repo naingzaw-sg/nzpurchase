fe3d8ec8ee3695febd0b596b5f49ec9cacc02940
naingzaw-sg <naingzawdigitalart@gmail.com>
2026-06-22 11:05:11 +0800

    Create products.js

---
+++ products.js
@@ -0,0 +1,43 @@
+// သင့်ရဲ့ ဖုန်းနံပါတ် နဲ့ Messenger Username
+const whatsappNumber = "959123456789"; 
+const messengerId = "yourpageusername"; 
+
+// 🌟 Product စာရင်းများ 🌟
+const products = [
+    {
+        brand: "Apple", // <-- Brand ကိုဒီလိုထည့်ပေးပါ
+        name: "iPhone 14 Pro Max",
+        condition: "Used (99%)",
+        type: "used", 
+        price: "2,500,000 MMK",
+        specs: "256GB Storage, Battery Health 95%, No error",
+        image: "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-14-pro-max-1.jpg"
+    },
+    {
+        brand: "Samsung",
+        name: "Galaxy S23 Ultra",
+        condition: "Brand New",
+        type: "new",
+        price: "3,200,000 MMK",
+        specs: "12GB RAM, 512GB Storage, Official Warranty",
+        image: "https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s23-ultra-5g-1.jpg"
+    },
+    {
+        brand: "Apple",
+        name: "iPad Air (5th Gen)",
+        condition: "Used (Good)",
+        type: "used",
+        price: "1,400,000 MMK",
+        specs: "64GB, Wi-Fi Only, Comes with Box",
+        image: "https://fdn2.gsmarena.com/vv/pics/apple/apple-ipad-air-5th-gen-2022-1.jpg"
+    },
+    {
+        brand: "Xiaomi",
+        name: "Redmi Note 12",
+        condition: "Brand New",
+        type: "new",
+        price: "450,000 MMK",
+        specs: "8GB RAM, 128GB Storage, Fast Charging",
+        image: "https://fdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-redmi-note-12-4g-1.jpg"
+    }
+];
